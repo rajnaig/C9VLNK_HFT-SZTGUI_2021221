b2daa868023e8b4828d8819df101c6d6b475cff3
Gábor Rajnai <gabika.hun@gmail.com>
2022-03-26 14:21:13 +0100

    Changed Directories

---
+++ C9VLNK_HFT_20211221.WpfClient/Services/Interfaces/ISongCreatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9VLNK_HFT_20211221.WpfClient.Services
+{
+    public interface ISongCreatorService
+    {
+    }
+}
---
+++ C9VLNK_HFT_20211221.WpfClient/ViewModel/SongCreatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9VLNK_HFT_20211221.WpfClient.ViewModel
+{
+    public class SongCreatorViewModel : ObservableRecipient
+    {
+        
+    }
+}
